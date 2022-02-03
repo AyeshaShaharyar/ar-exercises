73cdb306974296b448e884e9a02fb3941b9442b4
AyeshaShaharyar <ayesha2598@gmail.com>
2022-02-03 07:48:46 +0000

    Active Record practice exercises

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "Chanel")
+
+puts "Your new store name is #{Store.find(1).name}"
+
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@annual_revenue = Store.sum(:annual_revenue)
+puts @annual_revenue
+
+@average_revenue = Store.average(:annual_revenue)
+puts @average_revenue
+
+@one_million_more = Store.where('annual_revenue >= 1000000').count
+puts @one_million_more
